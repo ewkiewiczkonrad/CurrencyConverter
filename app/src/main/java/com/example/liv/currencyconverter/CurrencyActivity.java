820d412048de7cc768271ae7070ad094b60a278c
ewkiewiczkonrad <konrad.ewkiewicz@gmail.com>
2018-10-11 17:40:54 +0200

    Add new button, onClick, listView

--- app/src/main/java/com/example/liv/currencyconverter/CurrencyActivity.java
+++ app/src/main/java/com/example/liv/currencyconverter/CurrencyActivity.java
@@ -2,12 +2,59 @@ package com.example.liv.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class CurrencyActivity extends AppCompatActivity {
+
+    String[] NAMES = {"USD", "AUD","CAD","EUR","HUF","CHF","CHF","GBP","JPY","CZK","DKK","NOK","SEK","XDR"};
+    String SELL ="Sell";
+    String BUY= "Buy";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency);
+
+        ListView listview= (ListView) findViewById(R.id.listView);
+
+        CustomAdapter customAdapter=new CustomAdapter();
+        listview.setAdapter(customAdapter);
+    }
+
+    class CustomAdapter extends BaseAdapter{
+
+
+        @Override
+        public int getCount() {
+            return NAMES.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.customlayout, null);
+            TextView textView_name=(TextView)view.findViewById(R.id.textView_currency);
+            TextView textView_sell=(TextView)view.findViewById(R.id.textView_sell);
+            TextView textView_buy=(TextView)view.findViewById(R.id.textView_buy);
+
+            textView_name.setText(NAMES[position]);
+            textView_sell.setText(SELL);
+            textView_buy.setText(BUY);
+
+            return view;
+        }
     }
 }
--- app/src/main/java/com/example/liv/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/liv/currencyconverter/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.liv.currencyconverter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.button_currency)
+    Button buttonCurrency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+    }
+
+
+    @OnClick(R.id.button_currency)
+    public void onViewClicked() {
+        Intent intent = new Intent(this,CurrencyActivity.class);
+        startActivity(intent);
     }
 }
